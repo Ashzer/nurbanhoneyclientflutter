d5f18acba3eea4264ad52e1350a0877fa9788c6e
ashzer <inty.green@gmail.com>
2022-03-24 10:11:05 +0900

    board repository all tested

---
+++ lib/features/nurban_honey/domain/usecases/board_repository/get_boards.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+
+//change for each project
+import 'package:nurbanhoney_flutter/core/error/failures.dart';
+import 'package:nurbanhoney_flutter/core/usecase/usecase.dart';
+import 'package:nurbanhoney_flutter/features/nurban_honey/data/repositories/board_repository.dart';
+import 'package:nurbanhoney_flutter/features/nurban_honey/domain/entities/board_info/board_info.dart';
+
+class GetBoards implements UseCase<List<BoardInfo>, NoParams> {
+  final BoardRepository repository;
+  GetBoards(this.repository);
+
+  @override
+  Future<Either<Failure, List<BoardInfo>>> call(NoParams params) async {
+    return await repository.getBoards();
+  }
+}

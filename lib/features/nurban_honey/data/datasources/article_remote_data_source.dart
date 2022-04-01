b7aa01afc450edecbf80540ff39ac3449969b0e2
ashzer <inty.green@gmail.com>
2022-04-01 15:50:49 +0900

    need to fix token error(unathorized exception) before continue

--- lib/features/nurban_honey/data/datasources/article_remote_data_source.dart
+++ lib/features/nurban_honey/data/datasources/article_remote_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:nurbanhoney_flutter/features/nurban_honey/data/models/user_info.dart';
+
 import '../models/article_comment_model/article_comment_model.dart';
 import '../models/article_detail_model/article_detail_model.dart';
 import '../models/article_item_model/article_item_model.dart';
@@ -45,86 +50,99 @@ abstract class ArticleRemoteDataSource {
 class ArticleRemoteDataSourceImpl implements ArticleRemoteDataSource {
   final http.Client client;
   ArticleRemoteDataSourceImpl({required this.client});
+  static String baseUrl = "http://3.37.155.214:8128/board";
 
   @override
-  Future<EmptyResponseModel> deleteComment(
-      String address, String token, int commentId, int articleId) {
-    // TODO: implement deleteComment
-    throw UnimplementedError();
+  Future<List<ArticleItemModel>> getArticles(
+      String address, int flag, int offset, int limit) async {
+    final queryParams = {
+      'flag': '$flag',
+      'offset': '$offset',
+      'limit': '$limit'
+    };
+    final uri =
+        Uri.parse("$baseUrl/$address").replace(queryParameters: queryParams);
+    final response = await client.get(
+      uri,
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    final List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => ArticleItemModel.fromJson(e)).toList();
   }
 
   @override
-  Future<EmptyResponseModel> deleteDislike(
-      String address, String token, int articleId) {
-    // TODO: implement deleteDislike
+  Future<ArticleDetailModel> getArticle(
+      String address, String token, int articleId) async {
+    return ArticleDetailModel(1, "uuid", "thumbnail", "title", 2, "content", 3,
+        4, 5, 6, "updatedAt", 7, "badge", "nickname", ["insignia"], "myRating");
+  }
+
+  @override
+  Future<EmptyResponseModel> postLike(
+      String address, String token, int articleId) async {
+    // TODO: implement postLike
     throw UnimplementedError();
   }
 
   @override
   Future<EmptyResponseModel> deleteLike(
-      String address, String token, int articleId) {
+      String address, String token, int articleId) async {
     // TODO: implement deleteLike
     throw UnimplementedError();
   }
 
   @override
-  Future<ArticleDetailModel> getArticle(
-      String address, String token, int articleId) {
-    // TODO: implement getArticle
+  Future<EmptyResponseModel> postDislike(
+      String address, String token, int articleId) async {
+    // TODO: implement postDislike
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<EmptyResponseModel> deleteDislike(
+      String address, String token, int articleId) async {
+    // TODO: implement deleteDislike
     throw UnimplementedError();
   }
 
   @override
   Future<ArticleRatingsModel> getArticleRatings(
-      String address, String token, int articleId) {
+      String address, String token, int articleId) async {
     // TODO: implement getArticleRatings
     throw UnimplementedError();
   }
 
   @override
-  Future<List<ArticleItemModel>> getArticles(
-      String address, int flag, int offset, int limit) {
-    // TODO: implement getArticles
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<ArticleCommentModel> getComment(String address, int commentId) {
+  Future<ArticleCommentModel> getComment(String address, int commentId) async {
     // TODO: implement getComment
     throw UnimplementedError();
   }
 
   @override
   Future<List<ArticleCommentModel>> getComments(
-      String address, int articleId, int offset, int limit) {
+      String address, int articleId, int offset, int limit) async {
     // TODO: implement getComments
     throw UnimplementedError();
   }
 
   @override
   Future<EmptyResponseModel> postComment(
-      String address, String token, String comment, int articleId) {
+      String address, String token, String comment, int articleId) async {
     // TODO: implement postComment
     throw UnimplementedError();
   }
 
   @override
-  Future<EmptyResponseModel> postDislike(
-      String address, String token, int articleId) {
-    // TODO: implement postDislike
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<EmptyResponseModel> postLike(
-      String address, String token, int articleId) {
-    // TODO: implement postLike
+  Future<EmptyResponseModel> deleteComment(
+      String address, String token, int commentId, int articleId) async {
+    // TODO: implement deleteComment
     throw UnimplementedError();
   }
 
   @override
   Future<EmptyResponseModel> putComment(
-      String address, String token, int commentId, String comment) {
+      String address, String token, int commentId, String comment) async {
     // TODO: implement putComment
     throw UnimplementedError();
   }
--- test/features/nurban_honey/data/datasources/article_remote_data_source_test.dart
+++ test/features/nurban_honey/data/datasources/article_remote_data_source_test.dart
@@ -1,18 +1,65 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:nurbanhoney_flutter/features/nurban_honey/data/datasources/article_remote_data_source.dart';
+import 'package:nurbanhoney_flutter/features/nurban_honey/data/models/article_item_model/article_item_model.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
 import 'article_remote_data_source_test.mocks.dart';
 
 @GenerateMocks([http.Client])
 void main() {
-  ArticleRemoteDataSourceImpl dataSource;
-  MockClient mockHttpClient;
+  late ArticleRemoteDataSourceImpl dataSource;
+  late MockClient mockHttpClient;
   setUp(() {
     mockHttpClient = MockClient();
     dataSource = ArticleRemoteDataSourceImpl(client: mockHttpClient);
+  });
+  const String baseUrl = "http://3.37.155.214:8128/board";
+  group('Article remote data source tests:', () {
+    const String address = "address";
+    const String token = "token";
+    const flag = 0;
+    const offset = 0;
+    const limit = 10;
+    group('getArticles', () {
+      final List<dynamic> decode = jsonDecode(fixture("article_items.json"));
+      final tArticleItemModels =
+          decode.map((e) => ArticleItemModel.fromJson(e)).toList();
+      test(
+        "GET 요청으로 서버에서 Article 리스트 가져옴// flag(정렬 방식) , offset(시작 글번호) , limit(가져올 글 갯수) , application/json 헤더 포함",
+        () async {
+          when(mockHttpClient.get(any, headers: anyNamed('headers')))
+              .thenAnswer((_) async =>
+                  http.Response(fixture("article_items.json"), 200));
+
+          dataSource.getArticles(address, flag, offset, limit);
+
+          verify(mockHttpClient.get(
+              Uri.parse(
+                  "$baseUrl/$address?flag=$flag&offset=$offset&limit=$limit"),
+              headers: {'Content-Type': 'application/json'}));
+        },
+      );
+
+      test(
+        "response code가 200이면 article 리스트가 서버로부터 반환됨",
+        () async {
+          when(mockHttpClient.get(any, headers: anyNamed('headers')))
+              .thenAnswer((_) async =>
+                  http.Response(fixture("article_items.json"), 200));
+
+          final result =
+              await dataSource.getArticles(address, flag, offset, limit);
+
+          expect(result, equals(tArticleItemModels));
+        },
+      );
+    });
+    //TODO: Token(Unathorized Error 해결하고 돌아옴) Error처리부터 시작
   });
 }
